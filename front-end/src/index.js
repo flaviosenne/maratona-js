b30e53607db35f10ceaa44162027f0ce934473ca
flaviosenne <flaviosenne123@gmail.com>
2020-07-10 17:03:05 -0300

    Redux add

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider} from 'react-redux'
+
+import store from './store'
 
 import App from './app'
 import './styles/main.scss'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store = {store} >
+        <App/>
+    </Provider>,
+     document.getElementById('root')
+    )
--- front-end/src/screens/Signin/index.js
+++ front-end/src/screens/Signin/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const SignIn = () => {
+import  {connect}from 'react-redux'
+
+import {signIn} from './SigninActions'
+
+const SignIn = (props) => {
+
+    const { account, signIn} = props
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        
+        signIn({email: 'joao@email', password: '1234'})
+    }
+
+    console.log('**** SignIn.account', account)
     return (
         <div className = "container h-100 pt-5">
             <h1>Sign In</h1>
-            <div clssName = "d-flex flex-collumn h-100">
-                <form>
+            <div className = "d-flex flex-collumn h-100">
+                <form onSubmit = {submitHandler}>
                     <div className="form-group">
                         <label>Email</label>
                         <input type='text' className = 'form-control'></input>
@@ -30,4 +45,8 @@ const SignIn = () => {
     )
 }
 
-export default SignIn
+const mapStateToProps = (state) => {
+    return {account: state.signIn.account}
+}
+
+export default connect(mapStateToProps, {signIn})(SignIn)
