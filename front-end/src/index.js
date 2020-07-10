2d3df3f408ab6a89ed91d5b03b296f03880acfd4
flaviosenne <flaviosenne123@gmail.com>
2020-07-10 14:38:36 -0300

    Added ui-kit

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './app'
-import './main.scss'
+import '.styles/main.scss'
 
 ReactDOM.render(<App/>, document.getElementById('root'))
